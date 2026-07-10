2a2395ce71f3c21b9c13704a1d422e4201dbf6f0
Gabriel Delconti <gabrieldelconti99@gmail.com>
2026-07-10 15:32:25 +0000

    adding `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 The purpose of this project is to improve my web developing skills
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home () {
-  return <h1>So faith, hope, love remain, these three; but the greatest of these is love ✝️❤️‍🔥.</h1>
+function Home() {
+  return (
+    <h1>
+      So faith, hope, love remain, these three; but the greatest of these is
+      love ✝️❤️‍🔥
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
